53b18db2c7e424f5ab90b705c48c36bba3d0a917
chaikit <foreground.3ds@gmail.com>
2024-01-16 16:04:27 +0700

    Update

--- .ipynb_checkpoints/MetaTrade5-checkpoint.py
+++ .ipynb_checkpoints/MetaTrade5-checkpoint.py
@@ -2,24 +2,24 @@
 import MetaTrader5 as mt
 
 class MT:
-	def __init__(self,symbol=None,volume=None,login=None,password=None,server=None):
+	def __init__(self,ticker=None,volume=None):
 		if not ticker==None or volume==None:
-			self.symbol = symbol
+			self.ticker = ticker
 			self.volume = volume
-            self.login = login
-            self.password = password
-            self.server = server
 
+	login = '51560509'
+	password = 'C09mYA5@^F'
+	server = 'ICMarketsSC-Demo'
 	mt.initialize()
 	mt.login(login, password, server)
 
 	def OrderBuy(self,sl:float,tp:float,):
 		request = {
 			"action": mt.TRADE_ACTION_DEAL,
-			"symbol": self.symbol,
+			"symbol": self.ticker,
 			"volume": self.volume,
 			"type": mt.ORDER_TYPE_BUY,
-			"price": mt.symbol_info_tick(self.symbol).ask,
+			"price": mt.symbol_info_tick(self.ticker).ask,
 			'sl': sl,
 			'tp': tp,
 			'comment': 'Python Script Buy',
@@ -31,11 +31,11 @@ class MT:
 	def CancleBuy(self,pos:int):
 		request = {
 			"action": mt.TRADE_ACTION_DEAL,
-			"symbol": self.symbol,
+			"symbol": self.ticker,
 			"volume": self.volume,
 			"type": mt.ORDER_TYPE_SELL,
 			"position": pos,
-			"price": mt.symbol_info_tick(self.symbol).bid,
+			"price": mt.symbol_info_tick(self.ticker).bid,
 			'comment': 'Close Position',
 			'type_time': mt.ORDER_TIME_GTC,
 			'type_filling': mt.ORDER_FILLING_IOC,
@@ -45,10 +45,10 @@ class MT:
 	def OrderSell(self,sl:float,tp:float):
 		request = {
 			"action": mt.TRADE_ACTION_DEAL,
-			"symbol": self.symbol,
+			"symbol": self.ticker,
 			"volume": self.volume,
 			"type": mt.ORDER_TYPE_SELL,
-			"price": mt.symbol_info_tick(self.symbol).bid,
+			"price": mt.symbol_info_tick(ticker).bid,
 			'sl': sl,
 			'tp': tp,
 			'comment': 'Python Script Open',
@@ -57,43 +57,20 @@ class MT:
 		}
 		order = mt.order_send(request);print(order)
 
-	def CancleSell(self,pos:int):
+	def CancleSell(sel,pos:int):
 		request = {
 			"action": mt.TRADE_ACTION_DEAL,
-			"symbol": self.symbol,
+			"symbol": self.ticker,
 			"volume": self.volume,
 			"type": mt.ORDER_TYPE_BUY,
 			"position": pos,
-			"price": mt.symbol_info_tick(self.symbol).ask,
+			"price": mt.symbol_info_tick(self.ticker).ask,
 			'comment': 'Close Position',
 			'type_time': mt.ORDER_TIME_GTC,
 			'type_filling': mt.ORDER_FILLING_IOC,
 		}
 		order = mt.order_send(request);print(order)
 
-if __name__ == "__main__":
-    login = '51560509'
-    password = 'C09mYA5@^F'
-    server = 'ICMarketsSC-Demo'
-    symbol = 'ETHUSD'
-    volume = 0.01
-
-    #ประกาศตัวแปร
-    Trade = MT(symbol=symbol,volume=volume,login=login,password=password,server=server)
-    #ซื้อ
-    stoploss = 2255.0
-    takeprofit = 2266.0
-    Trade.OrderBuy(sl=stoploss,tp=takeprofit)
-    #ยกเลิกซื้อ
-    position = 559885613
-    Trade.CancleBuy(pos=position)
-    #ขาย
-    stoploss = 2265.0
-    takeprofit = 2250.0
-    Trade.OrderSell(sl=stoploss,tp=takeprofit)
-    #ยกเลิกขาย
-    position = 559885613
-    Trade.CancleSell(pos=position)
 
 
 '''
