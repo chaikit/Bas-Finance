699caeaad1d9817ac50de00870b5f84028d04775
chaikit <foreground.3ds@gmail.com>
2024-01-17 17:48:57 +0700

    Update-240117

--- Socket/Socket_Server.py
+++ Socket/Socket_Server.py
@@ -2,8 +2,9 @@ import socket
 
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(("192.168.1.70", 9090))
+serversocket.bind(("192.168.1.98", 9090))
 serversocket.listen(10)
+print('Wating for client..')
 
 connection, addr = serversocket.accept()
 print("[INFO]\tConnection stablished with:", addr)
